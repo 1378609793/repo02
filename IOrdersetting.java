8923bc43f9dde23b787adbd87ef2fe200a119aab
root <1378609793@qq.com>
2021-07-22 14:35:04 +0800

    IOrderSetting

---
+++ IOrdersetting.java
@@ -0,0 +1,9 @@
+package com.Shang.ServiceImpl;
+
+import com.Shang.pojo.OrderSetting;
+
+import java.util.List;
+
+public interface IOrdersetting {
+    public void add(List<OrderSetting> orderSetting);
+}
